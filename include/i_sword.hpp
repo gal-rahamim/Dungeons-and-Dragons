6c6aa1f083a8897306330b8bd92de0ba9551fba0
Gal Rahamim <galmail11@gmail.com>
2020-09-09 14:51:18 +0300

    making ISword methods inline

--- include/i_sword.hpp
+++ include/i_sword.hpp
@@ -8,12 +8,26 @@ namespace d_d {
 class ISword : public IObject {
 public:
     ISword(const std::string&, int a_strength);
+    virtual ~ISword();
     virtual void Describe(std::string& a_description) const = 0;
     int GetStrength() const;
 
 private:
     int m_strength;
 };
+
+inline ISword::ISword(const std::string& a_name, int a_strength)
+: IObject(a_name)
+, m_strength(a_strength)
+{
+}
+
+inline ISword::~ISword(){}
+
+inline int ISword::GetStrength() const
+{
+    return m_strength;
+}
 
 } //d_d
 
