e2020701f7a8e8d4c27b65c6cca62a15130f8cdc
Gal Rahamim <galmail11@gmail.com>
2020-09-13 11:01:17 +0300

    typo fix

--- src/shield.cpp
+++ src/shield.cpp
@@ -2,8 +2,8 @@
 
 namespace d_d {
 
-Shield::Shield(const std::string& a_name, const std::shared_ptr<IRoom>& a_start_pos, unsigned int a_respawn_sec, unsigned int a_strength)
-: IShield(a_name, a_start_pos, a_respawn_sec,a_strength)
+Shield::Shield(const std::string& a_name, const std::shared_ptr<IRoom>& a_start_pos, unsigned int a_respawn_sec, unsigned int a_defense)
+: IShield(a_name, a_start_pos, a_respawn_sec, a_defense)
 {}
 
 void Shield::Describe(std::string& a_description) const
